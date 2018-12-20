3143a8e10b3d1becc9f4dd35ca65f4f839fe8bdf
Eli Magallanes <emagalla@ucsd.edu>
2018-12-19 16:50:18 -0800

    Created a BookItem Vue to act as a child of the BookList;

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,25 +2,25 @@
     <div>
         <h1>{{ title }}</h1>
         <ul>
-            <li v-for="book in books">{{ book.title }} : {{book.author}}</li>
+            <li v-for="book in books">{{ book.title }} : {{ book.author }}</li>
         </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'BookList',
-        data() {
-            return {
-                title: 'All Books',
-                books: [
-                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-                    {title: 'American Gods', author: 'Neil Gaiman'},
-                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-                ]
-            };
-        }
+export default {
+    name: 'BookList',
+    data() {
+        return {
+            title: 'All Books',
+            books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+            ]
+        };
     }
+}
 </script>
 
 <style>
@@ -31,10 +31,5 @@
     ul {
         list-style-type: none;
         padding: 0;
-    }
-
-    li {
-        display: block;
-        margin: 0 10px;
     }
 </style>
